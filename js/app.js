aa35e0b7a460064cb94df4366ec9fa8cf935e246
Clayton de Oliveira <clayton.barrozo@gmail.com>
2017-01-30 22:42:23 +0000

    some updates

--- js/app.js
+++ js/app.js
@@ -11,7 +11,59 @@ function setBindings(){
 	
 	$("html body").animate({
 		scrollTop: $("#" + sectionId).offset().top
-		}, 1000)
+		}, 1000);
 
 	});
-} 
+}
+//Hide Header on Scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('nav').outerHeight();
+
+$(window).scroll(function(event){
+		didScroll = true;
+});
+
+setInterval(function(){
+	if (didScroll) {
+		hasScrolled();
+		didScroll = false;
+	}
+}, 250);
+
+
+function hasScrolled() {
+	var st = $(this).scrollTop();
+ // Make sure they scroll more than delta
+	if(Math.abs(lastScrollTop - st) <= delta)
+		return;
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+	if (st > lastScrollTop && st > navbarHeight){
+		$('nav').removeClass('nav-down').addClass('nav-up');
+		// Scroll Down
+	} else {
+		// Scroll Up
+		if (st + $(window).height() <$(document).height()) {
+			$('nav').removeClass('nav-up').addClass('nav-down');
+		}
+	}
+	lastScrollTop = st;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
